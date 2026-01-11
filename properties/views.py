e48f92046cff9ec2b209568a8222e60c7097fbf4
Sama Mostafa <samam6778@gmail.com>
2026-01-11 21:12:27 +0200

    Create views.py

---
+++ properties/views.py
@@ -0,0 +1,9 @@
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+from .utils import get_all_properties
+
+@cache_page(60 * 15)
+def property_list(request):
+    properties = get_all_properties()
+    data = list(properties.values())
+    return JsonResponse(data, safe=False)
