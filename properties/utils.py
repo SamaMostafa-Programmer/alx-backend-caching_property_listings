decf6c5f11a2665d87e91fc94a2981ac784d3356
Sama Mostafa <samam6778@gmail.com>
2026-01-11 21:16:01 +0200

    Add caching for all properties retrieval
    Implement caching for retrieving all properties.

---
+++ properties/utils.py
@@ -0,0 +1,13 @@
+from django.core.cache import cache
+from .models import Property
+from django_redis import get_redis_connection
+import logging
+
+logger = logging.getLogger(__name__)
+
+def get_all_properties():
+    properties = cache.get('all_properties')
+    if properties is None:
+        properties = Property.objects.all()
+        cache.set('all_properties', properties, 3600)
+    return properties
