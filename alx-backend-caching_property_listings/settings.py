a19a785f02b77f4c3e45622ca89fead22300bbd1
Sama Mostafa <samam6778@gmail.com>
2026-01-11 21:08:12 +0200

    Configure PostgreSQL database and Redis cache settings

--- alx-backend-caching_property_listings/settings.py
+++ alx-backend-caching_property_listings/settings.py
@@ -8,3 +8,22 @@ INSTALLED_APPS = [
     'django_redis',
     'properties',
 ]
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'properties_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    }
+}
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
