ced51076b1596e373acdcf8b8fd7d75fee629336
Sama Mostafa <samam6778@gmail.com>
2026-01-11 21:18:11 +0200

    Update utils.py

--- properties/utils.py
+++ properties/utils.py
@@ -11,3 +11,16 @@ def get_all_properties():
         properties = Property.objects.all()
         cache.set('all_properties', properties, 3600)
     return properties
+from django.core.cache import cache
+from .models import Property
+from django_redis import get_redis_connection
+import logging
+
+logger = logging.getLogger(__name__)
+
+def get_all_properties():
+    properties = cache.get('all_properties')
+    if properties is None:
+        properties = Property.objects.all()
+        cache.set('all_properties', properties, 3600)
+    return properties
